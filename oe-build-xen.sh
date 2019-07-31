befde3e2fa9385ed0888dabf53948a9fb65654c7
Christopher Clark <christopher.w.clark@gmail.com>
2019-07-31 16:38:11 -0700

    Add a command line opt for OE release and enable rerunning build
    Also just build xen and not the xen images for now; separate
    command line opts to select intended targets would make sense later.
    
    Signed-off-by: Christopher Clark <christopher.clark6@baesystems.com>

--- oe-build-xen.sh
+++ oe-build-xen.sh
@@ -21,7 +21,7 @@
 # TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 # SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-OE_RELEASE="thud"
+DEFAULT_OE_RELEASE="thud"
 SPEEDUP="--single-branch"
 REPOHOST_YOCTO="git.yoctoproject.org"
 REPOHOST_OE="git.openembedded.org"
@@ -30,8 +30,9 @@ REPOHOST_XEN="xenbits.xen.org"
 #---
 # Validate input
 
-if [ $# != 2 ] ; then
-    echo >&2 "Error: $0 <target arch> <xen branch>"
+if [ $# -lt 2 ] || [ $# -gt 3 ] ; then
+    echo >&2 "Error: $0 <target arch> <xen branch> [<open embedded branch>]"
+    echo >&2 "example: $0 x86-64 staging master"
     exit 1
 fi
 
@@ -43,6 +44,9 @@ case $1 in
 esac
 
 XEN_BRANCH="$2"
+
+OE_BRANCH="${DEFAULT_OE_RELEASE}"
+[ $# -lt 3 ] || OE_BRANCH="$3"
 
 #---
 # Determine Xen version information
@@ -69,40 +73,71 @@ fi
 
 #---
 # Obtain general build environment
-git clone --branch "${OE_RELEASE}" "${SPEEDUP}" "git://${REPOHOST_YOCTO}/poky"
-cd poky
-git clone --branch "${OE_RELEASE}" "${SPEEDUP}" "git://${REPOHOST_YOCTO}/meta-virtualization"
-git clone --branch "${OE_RELEASE}" "${SPEEDUP}" "git://${REPOHOST_OE}/meta-openembedded"
+obtain_build_materials() {
+    if ! git clone --branch "${OE_BRANCH}" "${SPEEDUP}" "git://${REPOHOST_YOCTO}/poky" ; then
+        echo >&2 "Error: failed to clone specified branch "${OE_BRANCH}" in the poky repository."
+        echo >&2 "Available branches are:"
+        git ls-remote --heads git://${REPOHOST_YOCTO}/poky | \
+            sed -ne 's/^.*refs\/heads\/\([^/]*\)$/\1/p' | \
+            grep -v '_' >&2
+        exit 4
+    fi
+    cd poky
+    git clone --branch "${OE_BRANCH}" "${SPEEDUP}" "git://${REPOHOST_YOCTO}/meta-virtualization"
+    git clone --branch "${OE_BRANCH}" "${SPEEDUP}" "git://${REPOHOST_OE}/meta-openembedded"
+    cd -
+}
 
+#---
+# tweaks
+tweak_build_materials() {
+    # meta-virtualization has a dependency on the selinux layer, but we're
+    # not building anything that requires it, so turn it off.
+    sed -i "poky/meta-virtualization/conf/layer.conf" -e '/^ *selinux \\$/d'
+
+}
+
+tweak_xen_recipe() {
+    # the ipxe recipe is currently broken but we don't need it, so turn that off.
+    sed -i "poky/meta-virtualization/recipes-extended/xen/xen.inc" -e 's/ipxe //'
+}
+
+#---
 # Initialize state and conf directory
-source ./oe-init-build-env build
+source_build_env() {
+    cd poky
+    source ./oe-init-build-env build
+}
 
 #---
 # bblayers.conf: add the necessary layers
-LAYER_ROOT="$(sed -ne 's/^  \(\/.*\/\)meta \\$/\1/p' <conf/bblayers.conf)"
-ESCAPED_LAYER_ROOT="$(echo $LAYER_ROOT | sed 's/\//\\\//g')"
-for LAYER in meta-virtualization \
-             meta-openembedded/meta-python \
-             meta-openembedded/meta-networking \
-             meta-openembedded/meta-filesystems \
-             meta-openembedded/meta-oe
-do
-    ESCAPED_LAYER="$(echo "${LAYER}" | sed 's/\//\\\//g')"
-    # Match on the meta-yocto-bsp layer line for the insertion point
-    sed "/^  ${ESCAPED_LAYER_ROOT}meta-yocto-bsp \\\\$/a\\ \\ ${ESCAPED_LAYER_ROOT}${ESCAPED_LAYER} \\\\" \
-        -i conf/bblayers.conf
-done
+write_layers_conf() {
+    LAYER_ROOT="$(sed -ne 's/^  \(\/.*\/\)meta \\$/\1/p' <conf/bblayers.conf)"
+    ESCAPED_LAYER_ROOT="$(echo $LAYER_ROOT | sed 's/\//\\\//g')"
+    for LAYER in meta-virtualization \
+                 meta-openembedded/meta-python \
+                 meta-openembedded/meta-networking \
+                 meta-openembedded/meta-filesystems \
+                 meta-openembedded/meta-oe
+    do
+        ESCAPED_LAYER="$(echo "${LAYER}" | sed 's/\//\\\//g')"
+        # Match on the meta-yocto-bsp layer line for the insertion point
+        sed "/^  ${ESCAPED_LAYER_ROOT}meta-yocto-bsp \\\\$/a\\ \\ ${ESCAPED_LAYER_ROOT}${ESCAPED_LAYER} \\\\" \
+            -i conf/bblayers.conf
+    done
+}
 
 #---
 # Since we're specifying the exact version of Xen that is to be built,
 # write a custom Xen recipe to do so.
 
-XEN_RECIPE_DIR="../meta-virtualization/recipes-extended/xen"
-rm -rf "${XEN_RECIPE_DIR}"
-mkdir "${XEN_RECIPE_DIR}"
-cd "${XEN_RECIPE_DIR}"
+write_xen_recipe() {
+    XEN_RECIPE_DIR="poky/meta-virtualization/recipes-extended/xen"
+    rm -rf "${XEN_RECIPE_DIR}"
+    mkdir "${XEN_RECIPE_DIR}"
+    cd "${XEN_RECIPE_DIR}"
 
-cat >xen_git.bb <<EOF
+    cat >xen_git.bb <<EOF
 require xen.inc
 
 SRCREV ?= "${XEN_SRCREV}"
@@ -118,33 +153,56 @@ S = "\${WORKDIR}/git"
 SRC_URI = "git://${REPOHOST_XEN}/xen.git;branch=\${XEN_BRANCH}"
 EOF
 
-# Obtain the dependent Xen recipes files from the tip of master:
-# they are maintained as backwards compatible.
-git checkout master -- xen.inc xen-arch.inc
+    # Obtain the dependent Xen recipes files from the tip of master:
+    # they are maintained as backwards compatible.
+    git checkout master -- xen.inc xen-arch.inc
+    cd -
+}
 
-cd -
+#---
+# TODO: enable shared sstate cache
+
+#---
+# TODO: enable the download directory
 
 #---
 # local.conf: add the necessary variable definitions
 
-[ -f "conf/local.conf.template" ] || mv conf/local.conf conf/local.conf.template
-cat conf/local.conf.template - >conf/local.conf <<EOF
+write_local_conf() {
+    PROCESSOR_COUNT="$(cat /proc/cpuinfo | grep '^processor\s*:' | wc -l)"
+
+    [ -f "conf/local.conf.template" ] || mv conf/local.conf conf/local.conf.template
+    cat conf/local.conf.template - >conf/local.conf <<EOF
 
 MACHINE = "${MACHINE}"
 DISTRO_FEATURES_append = " virtualization xen"
 
-PREFERRED_VERSION_xen = "${XEN_REL_VALUE}+git%"
+PREFERRED_VERSION_xen = "${XEN_REL}+git%"
 
 # Enable parallelism in the build. These values should be tuned
 # to appropriate values for the build host.
-BB_NUMBER_THREADS ?= "8"
-PARALLEL_MAKE ?= "-j 4"
+BB_NUMBER_THREADS ?= "${PROCESSOR_COUNT}"
+PARALLEL_MAKE ?= "-j 2"
 EOF
+}
 
 #---
-# TODO: enable shared sstate cache and the download binary mirror here
+if [ ! -e poky ] ; then
+    obtain_build_materials
+    tweak_build_materials
+    write_xen_recipe
+    tweak_xen_recipe
+    source_build_env
+    write_layers_conf
+    write_local_conf
+else
+    # TODO: verify that MACHINE, XEN_BRANCH and OE_BRANCH match the tree
+    source_build_env
+fi
 
 #---
 # Run the build
 
-bitbake xen && bitbake xen-image-minimal && bitbake xen-guest-image-minimal
+bitbake xen
+# TODO: optional full build:
+# bitbake xen && xen-image-minimal && bitbake xen-guest-image-minimal
